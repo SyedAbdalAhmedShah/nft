9fe96b81ca86bcd36d438995e9706d06d569cceb
Syed Abdal SHah <syedahmad123123shah@gmail.com>
2024-06-06 03:05:51 +0500

    image assets added

---
+++ lib/constants/app_assets.dart
@@ -0,0 +1,7 @@
+// ignore_for_file: constant_identifier_names
+
+class AppAssets {
+  static const String IMAGE_PATH = "assets/images/";
+  static const String SPLASH_BACKGROUND_IMG =
+      "${IMAGE_PATH}javier-miranda-MrWOCGKFVDg-unsplash 1.png";
+}
--- lib/views/splash.dart
+++ lib/views/splash.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:nft/constants/app_assets.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return DecoratedBox(
-      decoration: BoxDecoration(),
+    return const DecoratedBox(
+      decoration: BoxDecoration(
+          image: DecorationImage(image: AssetImage(AppAssets.IMAGE_PATH))),
       child: Scaffold(
         body: Column(),
       ),
