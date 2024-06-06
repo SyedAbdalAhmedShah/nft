d9c49784aa8edb1b06d03ae19879d11b08932647
Syed Abdal SHah <syedahmad123123shah@gmail.com>
2024-06-07 01:22:13 +0500

    slider maade

--- lib/constants/app_strings.dart
+++ lib/constants/app_strings.dart
@@ -2,6 +2,9 @@
 
 class AppStrings {
   static const String APP_NAME = "NFT Marketplace";
+  static const String MUSIC = "Music";
+  static const String ART = "Art";
+  static const String VIRTUAL_WORLD = "Virtual World";
   static const String EXPLORE_NFT = "Explore and Mint NFTs";
   static const String YOU_CAN_BUY =
       "You can buy and sell the NFTs of the best artists in the world";
--- lib/views/bottom_nav_bar.dart
+++ lib/views/bottom_nav_bar.dart
@@ -1,18 +1,24 @@
+import 'dart:ui';
+
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:nft/constants/app_assets.dart';
 import 'package:nft/constants/app_strings.dart';
+import 'package:nft/models/slider_model.dart';
 
 class BottomNavBarScreen extends StatelessWidget {
   const BottomNavBarScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final List<String> sliderImages = [
-      AppAssets.MUSIC_SLIDER_IMG,
-      AppAssets.ART_SLIDER_IMG,
-      AppAssets.WRORLD_SLIDER_IMG
+    final List<SliderModel> sliderImages = [
+      SliderModel(
+          imagePath: AppAssets.MUSIC_SLIDER_IMG, name: AppStrings.MUSIC),
+      SliderModel(imagePath: AppAssets.ART_SLIDER_IMG, name: AppStrings.ART),
+      SliderModel(
+          imagePath: AppAssets.WRORLD_SLIDER_IMG,
+          name: AppStrings.VIRTUAL_WORLD),
     ];
     return Scaffold(
       appBar: AppBar(
@@ -25,22 +31,39 @@ class BottomNavBarScreen extends StatelessWidget {
       body: Column(
         children: [
           CarouselSlider(
-            options: CarouselOptions(height: 170.h),
+            options: CarouselOptions(
+              height: 170.h,
+              autoPlay: true,
+              enlargeCenterPage: true,
+            ),
             items: sliderImages.map((i) {
               return Builder(
                 builder: (BuildContext context) {
                   return Container(
                       width: MediaQuery.of(context).size.width,
-                      margin: EdgeInsets.symmetric(horizontal: 5.0),
+                      margin: const EdgeInsets.symmetric(horizontal: 5.0),
                       decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(28.r),
                           image: DecorationImage(
-                              image: AssetImage(i), fit: BoxFit.cover)),
+                              image: AssetImage(i.imagePath),
+                              fit: BoxFit.cover)),
                       child: Align(
                         alignment: Alignment.bottomCenter,
-                        child: Text(
-                          'text $i',
-                          style: TextStyle(fontSize: 16.0),
+                        child: ClipRRect(
+                          child: BackdropFilter(
+                            filter: ImageFilter.blur(sigmaX: 2, sigmaY: 2),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  i.name,
+                                  style: TextStyle(
+                                      fontSize: 20.0.sp,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
+                          ),
                         ),
                       ));
                 },
