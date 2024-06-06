744ba293441c0c42993d57091ba5375723469ea7
Syed Abdal SHah <syedahmad123123shah@gmail.com>
2024-06-07 01:56:20 +0500

    nav bar design added

--- lib/views/bottom_nav_bar.dart
+++ lib/views/bottom_nav_bar.dart
@@ -1,13 +1,19 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:nft/constants/app_strings.dart';
 
-class BottomNavBarScreen extends StatelessWidget {
+class BottomNavBarScreen extends StatefulWidget {
   const BottomNavBarScreen({super.key});
 
+  @override
+  State<BottomNavBarScreen> createState() => _BottomNavBarScreenState();
+}
+
+class _BottomNavBarScreenState extends State<BottomNavBarScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,15 +24,70 @@ class BottomNavBarScreen extends StatelessWidget {
           style: TextStyle(fontSize: 26.sp, fontWeight: FontWeight.bold),
         ),
       ),
-      bottomNavigationBar: ClipRRect(
-          borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(70.r), topRight: Radius.circular(70.r)),
-          child: BackdropFilter(
-            filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
-            child: BottomAppBar(
-              color: Theme.of(context).colorScheme.primary.withOpacity(0.2),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked     ,
+      floatingActionButton: FloatingActionButton(onPressed: () {}),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.transparent,
+
+        padding: EdgeInsets.zero,
+        // shape: const CircularNotchedRectangle(),
+        // notchMargin: 20.0,
+        clipBehavior: Clip.antiAlias,
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+          child: DecoratedBox(
+            decoration: BoxDecoration(
+                border: Border.all(
+                    color: Theme.of(context)
+                        .colorScheme
+                        .secondary
+                        .withOpacity(0.2),
+                    width: 1.w),
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(60.r),
+                    topRight: Radius.circular(60.r)),
+                gradient: LinearGradient(
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                    colors: [
+                      Theme.of(context).colorScheme.secondary.withOpacity(0.2),
+                      const Color(0xff420C5B).withOpacity(0.4)
+                    ])),
+            child: SizedBox(
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  IconButton(
+                    icon: const Icon(Icons.home),
+                    onPressed: () {
+                      setState(() {});
+                    },
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.search),
+                    onPressed: () {
+                      setState(() {});
+                    },
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.favorite_border_outlined),
+                    onPressed: () {
+                      setState(() {});
+                    },
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.account_circle_outlined),
+                    onPressed: () {
+                      setState(() {});
+                    },
+                  )
+                ],
+              ),
             ),
-          )),
+          ),
+        ),
+      ),
     );
   }
 }
