95966c17f4df828ec1c29376cae45783346b0b3b
Syed Abdal SHah <syedahmad123123shah@gmail.com>
2024-06-10 17:54:37 +0500

    nft detail page completed

--- lib/views/nft_detail_page.dart
+++ lib/views/nft_detail_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gap/gap.dart';
+import 'package:nft/constants/app_assets.dart';
+import 'package:nft/constants/app_colors.dart';
 
 class NftDetailPage extends StatelessWidget {
   final String imagePath;
@@ -7,23 +11,116 @@ class NftDetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("data"),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            flex: 1,
-            child: ColoredBox(color: Theme.of(context).colorScheme.primary),
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
-              decoration: BoxDecoration(
-                  image: DecorationImage(image: AssetImage(imagePath))),
+      body: SafeArea(
+        bottom: false,
+        child: Column(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Align(
+                      alignment: Alignment.centerRight,
+                      child: IconButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          icon: const Icon(Icons.cancel))),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20.0.w),
+                    child: Text(
+                      "Luppy Club",
+                      style: TextStyle(
+                          fontSize: 18.sp, fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                  Gap(5.h),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20.0.w),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        CircleAvatar(
+                          backgroundImage: AssetImage(AppAssets.NFT_1),
+                        ),
+                        Text("Alex.S")
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-        ],
+            Expanded(
+              flex: 5,
+              child: Container(
+                width: double.infinity,
+                padding: EdgeInsets.all(20.r),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(50.r),
+                      topRight: Radius.circular(50.r),
+                    ),
+                    color: Colors.white,
+                    image: DecorationImage(
+                      image: AssetImage(imagePath),
+                      fit: BoxFit.fill,
+                    )),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Gap(10.h),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const CircleAvatar(
+                          backgroundColor: AppColorss.BACKGROUND_2,
+                          child: Icon(
+                            Icons.qr_code_rounded,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            const CircleAvatar(
+                              backgroundColor: AppColorss.BACKGROUND_2,
+                              child: Icon(
+                                Icons.share,
+                                color: Colors.white,
+                              ),
+                            ),
+                            Gap(10.w),
+                            Chip(
+                              backgroundColor: AppColorss.BACKGROUND_2,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20.r),
+                                  side: const BorderSide(
+                                      color: Colors.transparent)),
+                              label: const Text("200"),
+                              avatar: const Icon(
+                                Icons.favorite,
+                                color: Colors.red,
+                              ),
+                            ),
+                            Gap(10.w),
+                            const CircleAvatar(
+                              backgroundColor: AppColorss.BACKGROUND_2,
+                              child: Icon(
+                                Icons.share,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
